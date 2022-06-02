44e11e79c28ebde9a288587a154d0ebb765a7d21
Parkseonho <tjsgh2103@gmail.com>
2022-06-02 11:09:17 +0900

    about 페이지 생성

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main>
+      <title>About</title>
+      <h1>🐈꼬솜이를 소개합니다🐈</h1>
+      <p> 😊 탄생일 : 2021-04-15</p>
+    </main>
+  );
+};
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
-      <h1>안뇽 hi</h1>
+      <h1>안뇽</h1>
+      <p>ㅋ</p>
     </main>
   );
 };
